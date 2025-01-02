7d6366ed345a62a22c708cf6811d91c36e733662
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2025-01-02 14:20:07 +0000

    aa

--- src/components/ButtonCounter.jsx
+++ src/components/ButtonCounter.jsx
@@ -8,7 +8,7 @@ const[count,setCount]=useState(0)
   return (
     <div>
 
-<p> Button clicked {count} time</h2>
+<p> Button clicked {count} time</p>
         <button  onClick={()=>{
          
           setCount(count+1)
